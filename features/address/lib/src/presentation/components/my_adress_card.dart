df635667bb0608f990316899fd88997145557ae0
Eduardo Muniz <90477293+eduardohr-muniz@users.noreply.github.com>
2025-08-08 18:56:55 -0300

    my address -card

--- features/address/lib/src/presentation/components/my_adress_card.dart
+++ features/address/lib/src/presentation/components/my_adress_card.dart
@@ -33,79 +33,73 @@ class _MyAdressCardState extends State<MyAdressCard> {
 
   @override
   Widget build(BuildContext context) {
-    return ArtTheme(
-      data: ArtThemeData(
-        colorScheme: ArtBlueColorScheme.dark(),
-        brightness: Brightness.dark,
-      ),
-      child: ArtCard(
-        padding: EdgeInsets.zero,
-        border: widget.isSelected ? Border.all(color: context.artColorScheme.ring, width: 1.2) : null,
-        child: Stack(
-          children: [
-            Padding(
-              padding: PSize.spacer.paddingHorizontal + PSize.ii.paddingVertical,
-              child: Row(
-                children: [
-                  PaipIcon(PaipIcons.homeBold, color: context.artColorScheme.mutedForeground),
-                  SizedBox(width: 22),
-                  Flexible(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(_address.nickName, style: context.artTextTheme.h2.copyWith(fontSize: 16)),
-                        SizedBox(height: 4),
-                        Text(_address.mainText),
-                        Text(_address.secondaryText, style: context.artTextTheme.muted),
-                        Text(_address.complement, style: context.artTextTheme.muted.copyWith(fontSize: 12, color: context.artColorScheme.primary)),
-                      ],
-                    ),
+    return ArtCard(
+      padding: EdgeInsets.zero,
+      border: widget.isSelected ? Border.all(color: context.artColorScheme.ring, width: widget.isSelected ? 2 : 0) : null,
+      child: Stack(
+        children: [
+          Padding(
+            padding: PSize.spacer.paddingHorizontal + PSize.ii.paddingVertical,
+            child: Row(
+              children: [
+                PaipIcon(PaipIcons.homeBold, color: context.artColorScheme.mutedForeground),
+                SizedBox(width: 22),
+                Flexible(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(_address.nickName, style: context.artTextTheme.h2.copyWith(fontSize: 16)),
+                      SizedBox(height: 4),
+                      Text(_address.mainText),
+                      Text(_address.secondaryText, style: context.artTextTheme.muted),
+                      Text(_address.complement, style: context.artTextTheme.muted.copyWith(fontSize: 12, color: context.artColorScheme.primary)),
+                    ],
                   ),
-                  SizedBox(width: 40), // Espaço para o botão
-                ],
-              ),
+                ),
+                SizedBox(width: 40), // Espaço para o botão
+              ],
             ),
-            if (widget.onEdit != null || widget.onDelete != null)
-              Positioned(
-                top: 6,
-                right: 6,
-                child: ArtContextMenu(
-                  controller: _popoverController,
-                  items: [
-                    if (widget.onEdit != null)
-                      ArtContextMenuItem(
-                        leading: PaipIcon(
-                          PaipIcons.editBold,
-                          size: 18,
-                          color: context.artColorScheme.foreground,
-                        ),
-                        child: Text('Editar'),
-                        onPressed: () => widget.onEdit?.call(_address),
+          ),
+          if (widget.onEdit != null || widget.onDelete != null)
+            Positioned(
+              top: 6,
+              right: 6,
+              child: ArtContextMenu(
+                controller: _popoverController,
+                items: [
+                  if (widget.onEdit != null)
+                    ArtContextMenuItem(
+                      leading: PaipIcon(
+                        PaipIcons.editBold,
+                        size: 18,
+                        color: context.artColorScheme.foreground,
                       ),
-                    if (widget.onDelete != null)
-                      ArtContextMenuItem(
-                        leading: PaipIcon(
-                          PaipIcons.deleteDuotone,
-                          size: 18,
-                          color: context.artColorScheme.destructive,
-                        ),
-                        child: Text('Excluir'),
-                        onPressed: () => widget.onDelete?.call(_address),
-                      ),
-                  ],
-                  child: ArtIconButton.ghost(
-                    onPressed: () {
-                      _popoverController.show();
-                    },
-                    icon: Icon(
-                      Icons.more_vert,
-                      color: context.artColorScheme.ring,
+                      child: Text('Editar'),
+                      onPressed: () => widget.onEdit?.call(_address),
                     ),
+                  if (widget.onDelete != null)
+                    ArtContextMenuItem(
+                      leading: PaipIcon(
+                        PaipIcons.deleteDuotone,
+                        size: 18,
+                        color: context.artColorScheme.destructive,
+                      ),
+                      child: Text('Excluir'),
+                      onPressed: () => widget.onDelete?.call(_address),
+                    ),
+                ],
+                child: ArtIconButton.ghost(
+                  onPressed: () {
+                    _popoverController.show();
+                  },
+                  icon: Icon(
+                    Icons.more_vert,
+                    color: context.artColorScheme.ring,
                   ),
                 ),
               ),
-          ],
-        ),
+            ),
+        ],
       ),
     );
   }
